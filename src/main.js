bd3baba35c30dd1f13b80507cc8ded77f8619a6c
yxp <2878847069@qq.com>
2019-05-14 19:39:21 +0800

    feature: 座位图整合集中

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 自定义组件
+import Seat from '@/views/Seat'
+Vue.component('seat', Seat)
 
 Vue.config.productionTip = false
 
